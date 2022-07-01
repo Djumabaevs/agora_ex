621ad455bf00e2641519cef71b578658bc84977c
Djumabaevs <djumabaevb@gmail.com>
2022-07-01 22:31:43 +0600

    chat screen

--- lib/chat/views/chat_screen.dart
+++ lib/chat/views/chat_screen.dart
@@ -5,6 +5,17 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat'),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.only(bottom: 16),
+        reverse: true,
+        itemBuilder: (_, index) => const SizedBox.shrink(),
+        separatorBuilder: (_, __) => const SizedBox(height: 16),
+        itemCount: 0,
+      ),
+    );
   }
 }
