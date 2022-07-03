1b25225c1fb3698f8256894c5e77c54fc8ccc8fd
Djumabaevs <djumabaevb@gmail.com>
2022-07-03 10:46:12 +0600

    message bubboe design

--- lib/chat/widgets/message_bubble.dart
+++ lib/chat/widgets/message_bubble.dart
@@ -19,7 +19,20 @@ class MessageBubble extends StatelessWidget {
       mainAxisAlignment:
           isReceiver ? MainAxisAlignment.start : MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.end,
-      children: const [],
+      children: [
+        Container(
+          padding: const EdgeInsets.all(12),
+          constraints: const BoxConstraints(maxWidth: 275),
+          decoration: BoxDecoration(
+            color: isReceiver ? Colors.green : Colors.blue,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(
+            message,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
     );
   }
 }
