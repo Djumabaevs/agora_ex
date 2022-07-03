833763b36a0e44878b83588d80423a9326808005
Djumabaevs <djumabaevb@gmail.com>
2022-07-03 11:04:03 +0600

    profile image of reciever in chat

--- lib/chat/widgets/message_bubble.dart
+++ lib/chat/widgets/message_bubble.dart
@@ -20,16 +20,37 @@ class MessageBubble extends StatelessWidget {
           isReceiver ? MainAxisAlignment.start : MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        Container(
-          padding: const EdgeInsets.all(12),
-          constraints: const BoxConstraints(maxWidth: 275),
-          decoration: BoxDecoration(
-            color: isReceiver ? Colors.green : Colors.blue,
-            borderRadius: BorderRadius.circular(10),
+        if (isReceiver)
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: CircleAvatar(
+              backgroundColor: Colors.grey[200],
+              foregroundImage: NetworkImage(profileImageUrl!),
+            ),
           ),
-          child: Text(
-            message,
-            style: const TextStyle(color: Colors.white),
+        Flexible(
+          child: Column(
+            crossAxisAlignment:
+                isReceiver ? CrossAxisAlignment.start : CrossAxisAlignment.end,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(12),
+                constraints: const BoxConstraints(maxWidth: 275),
+                decoration: BoxDecoration(
+                  color: isReceiver ? Colors.green : Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  message,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                date,
+                style: const TextStyle(fontSize: 12),
+              ),
+            ],
           ),
         ),
       ],
