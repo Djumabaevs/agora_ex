b8f3bc65b609273c99ad408530991488f72dcd33
Djumabaevs <djumabaevb@gmail.com>
2022-07-04 10:47:41 +0600

    text field with decoration

--- lib/chat/widgets/message_text_field.dart
+++ lib/chat/widgets/message_text_field.dart
@@ -5,6 +5,25 @@ class MessageTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: TextField(
+          textAlignVertical: TextAlignVertical.center,
+          textCapitalization: TextCapitalization.sentences,
+          textInputAction: TextInputAction.send,
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 16,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            hintText: 'Send a message',
+          ),
+        ),
+      ),
+    );
   }
 }
